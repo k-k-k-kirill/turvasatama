7014091b8d1d639066a78d66546fe10d0af62a87
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2022-06-13 22:06:28 +0300

    about

--- web/app/themes/turvasatama-theme/controllers/template-sectioned.php
+++ web/app/themes/turvasatama-theme/controllers/template-sectioned.php
@@ -7,6 +7,10 @@
  */
 
 use Pixels\Theme\Controllers\PostController;
+use Pixels\TurvaSatama\App;
+
+// Services
+$postService = App::$container->get('post');
 
 // Set up Controller instance.
 $controller = new PostController();
@@ -16,6 +20,7 @@ $controller->set_templates( 'template/template-sectioned.twig' );
 
 // Get flexible fields.
 $sections = get_field( 'sectioned_fields', get_the_id() );
+$sections = $postService->injectFeedData($sections);
 $controller->add_context( 'sections', $sections );
 
 // Render the twig.
